64bd1ef86168816c6cfe112854f5e9f55489c904
Wilhelm Kirschbaum <wkirschbaum@gmail.com>
2015-06-21 18:50:09 +0200

    clean run_analysis script

--- run_analysis.R
+++ run_analysis.R
@@ -1,16 +1,16 @@
 library(dplyr)
 
 #load all required data
-if(!exists('activity_labels')){activity_labels <- read.table("./UCI HAR Dataset/activity_labels.txt")}
-if(!exists('features')){features <- read.table("./UCI HAR Dataset/features.txt")}
+activity_labels <- read.table("./UCI HAR Dataset/activity_labels.txt")
+features <- read.table("./UCI HAR Dataset/features.txt")
 
-if(!exists("x_test")){x_test <- read.table("./UCI HAR Dataset/test/X_test.txt")}
-if(!exists("y_test")){y_test <- read.table("./UCI HAR Dataset/test/y_test.txt")}
-if(!exists("subject_test")){subject_test <- read.table("./UCI HAR Dataset/test/subject_test.txt")}
+x_test <- read.table("./UCI HAR Dataset/test/X_test.txt")
+y_test <- read.table("./UCI HAR Dataset/test/y_test.txt")
+subject_test <- read.table("./UCI HAR Dataset/test/subject_test.txt")
 
-if(!exists("x_train")){x_train <- read.table("./UCI HAR Dataset/train/X_train.txt")}
-if(!exists("y_train")){y_train <- read.table("./UCI HAR Dataset/train/y_train.txt")}
-if(!exists("subject_train")){subject_train <- read.table("./UCI HAR Dataset/train/subject_train.txt")}
+x_train <- read.table("./UCI HAR Dataset/train/X_train.txt")
+y_train <- read.table("./UCI HAR Dataset/train/y_train.txt")
+subject_train <- read.table("./UCI HAR Dataset/train/subject_train.txt")
 
 #combine training and test data
 x_combined <- rbind(x_test, x_train)
